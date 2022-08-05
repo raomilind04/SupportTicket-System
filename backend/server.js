d471a274df694bce6d55231bf6c57730d1eb8478
raomilind04 <72732775+raomilind04@users.noreply.github.com>
2022-08-05 22:25:05 +0530

    add login and register routes

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-console.log("Server");
+const express= require("express"); 
+const dotenv= require("dotenv").config(); 
+const PORT= process.env.PORT; 
+
+const app= express(); 
+
+app.get("/", (req,res)=> {
+    res.send("Hello")
+})
+app.use("/api/users", require("./routes/userRoutes")); 
+
+app.listen(PORT, console.log(`Server Started at port ${PORT}`)); 
