73cda0d4e03a29d844c82a4f9545994eebd74da4
raomilind04 <72732775+raomilind04@users.noreply.github.com>
2022-08-06 22:51:21 +0530

    add jwt

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,10 @@
-const asyncHandler= require("express-async-handler"); 
-const bcrypt= require("bcryptjs"); 
+const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken"); 
 
-const User= require("../models/userModel"); 
+const User = require("../models/userModel");
 
-
-const registerUser = asyncHandler( async (req, res) => {
+const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
 
   if (!name || !email || !password) {
@@ -12,36 +12,54 @@ const registerUser = asyncHandler( async (req, res) => {
     throw new Error("Incomeplete Data");
   }
 
-  const userExits = await User.findOne({email}); 
-  if(userExits){
-    res.status(400); 
-    throw new Error("User already Exist"); 
+  const userExits = await User.findOne({ email });
+  if (userExits) {
+    res.status(400);
+    throw new Error("User already Exist");
   }
 
-  const salt= await bcrypt.genSalt(10); 
-  const hashedPassword= await bcrypt.hash(password, salt); 
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
 
-  const user= await User.create({
-    name: name, 
-    email: email, 
-    password: hashedPassword
-  }); 
-  if(user){
+  const user = await User.create({
+    name: name,
+    email: email,
+    password: hashedPassword,
+  });
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id)
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user name");
+  }
+});
+
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user && (await bcrypt.compare(password, user.password))) {
     res.status(200).json({
-        _id: user._id, 
-        name: user.name, 
-        email: user.email 
-
-    })
-  }else {
-    res.status(400); 
-    throw new Error("Invalid user name")
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id)
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid Credentials");
   }
+});
 
-});
-const loginUser = asyncHandler (async (req, res) => {
-  res.send("Login Route");
-});
+const generateToken= (id)=> {
+    return jwt.sign({id}, process.env.JWT_SECRET, {
+        expiresIn: "30d"
+    })
+}
 
 module.exports = {
   registerUser,
