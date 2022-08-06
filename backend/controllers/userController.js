30aa214a168b30b7dff2c6ff7b8f3fc4179e5bae
raomilind04 <72732775+raomilind04@users.noreply.github.com>
2022-08-06 22:21:31 +0530

    add user register and bcrypt for password

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,7 @@
 const asyncHandler= require("express-async-handler"); 
+const bcrypt= require("bcryptjs"); 
+
+const User= require("../models/userModel"); 
 
 
 const registerUser = asyncHandler( async (req, res) => {
@@ -9,7 +12,32 @@ const registerUser = asyncHandler( async (req, res) => {
     throw new Error("Incomeplete Data");
   }
 
-  res.send("Register Route");
+  const userExits = await User.findOne({email}); 
+  if(userExits){
+    res.status(400); 
+    throw new Error("User already Exist"); 
+  }
+
+  const salt= await bcrypt.genSalt(10); 
+  const hashedPassword= await bcrypt.hash(password, salt); 
+
+  const user= await User.create({
+    name: name, 
+    email: email, 
+    password: hashedPassword
+  }); 
+  if(user){
+    res.status(200).json({
+        _id: user._id, 
+        name: user.name, 
+        email: user.email 
+
+    })
+  }else {
+    res.status(400); 
+    throw new Error("Invalid user name")
+  }
+
 });
 const loginUser = asyncHandler (async (req, res) => {
   res.send("Login Route");
