827aa018812129ea9a5d0f893472e24c7aca1947
raomilind04 <72732775+raomilind04@users.noreply.github.com>
2022-08-07 23:04:39 +0530

    add ui for the home page

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,6 +1,20 @@
+import {Link} from "react-router-dom" 
+import {FaQuestionCircle, FaTicketAlt} from "react-icons/fa"; 
+
 function Home(){
     return(
-        <div>Home</div>
+        <>
+            <section className="heading">
+                <h1>How can we help you ?</h1>
+                <p>Please choose from the options below</p>
+            </section>
+            <Link to="/new-ticket" className="btn btn-reverse btn-block">
+                <FaQuestionCircle /> Create New Ticket
+            </Link>
+            <Link to="/new-ticket" className="btn btn-block">
+                <FaTicketAlt /> My tickets
+            </Link>
+        </>
     )
 }
 
