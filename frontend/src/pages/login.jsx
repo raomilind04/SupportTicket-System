14e04e0ae9d5610a63ee5881c2efe7d3a9e3a28e
raomilind04 <72732775+raomilind04@users.noreply.github.com>
2022-08-07 22:59:19 +0530

    add the ui for login page

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,7 +1,65 @@
-function Login(){
-    return(
-        <div>Login</div>
-    )
+import { useState } from "react";
+import { FaUser, FaSignInAlt } from "react-icons/fa";
+import { toast } from "react-toastify";
+
+function Login() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const { email, password } = formData;
+
+  const onChange = (e) => {
+    setFormData((prevState) => {
+      return {
+        ...prevState,
+        [e.target.name]: e.target.value,
+      };
+    });
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <section className="heading">
+        <h1>
+          <FaSignInAlt /> Login
+        </h1>
+        <p>Please Login</p>
+      </section>
+      <section className="form">
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input
+              className="form-control"
+              name="email"
+              type="email"
+              id="email"
+              value={email}
+              onChange={onChange}
+              placeholder="Email"
+              required
+            />
+            <input
+              className="form-control"
+              name="password"
+              type="password"
+              id="password"
+              value={password}
+              onChange={onChange}
+              placeholder="Password"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <button className="btn btn-block">Submit</button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
 }
 
-export default Login; 
+export default Login;
