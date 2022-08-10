2c0c377d4aa00f26fc3fe929302b36b03ac0f4a6
raomilind04 <72732775+raomilind04@users.noreply.github.com>
2022-08-10 23:54:08 +0530

    fetch user tickets from the backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import {ToastContainer} from "react-toastify"; 
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 import Home from "./pages/home";
 import Login from "./pages/login";
 import Register from "./pages/register";
 import NewTicket from "./pages/newticket";
 import PrivateRoute from "./components/privateRoute";
+import Tickets from "./pages/tickets";
 
 import Header from "./components/header";
 
@@ -22,6 +23,9 @@ function App() {
             <Route path="/register" element={<Register />} />
             <Route path="/new-ticket" element={<PrivateRoute />}>
               <Route path="/new-ticket" element={<NewTicket />} />
+            </Route>
+            <Route path="/tickets" element={<PrivateRoute />}>
+              <Route path="/tickets" element={<Tickets />} />
             </Route>
           </Routes>
         </div>
