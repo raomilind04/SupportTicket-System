9827a4b73e415f2f374a7645decd0a7020128926
raomilind04 <72732775+raomilind04@users.noreply.github.com>
2022-08-11 12:21:29 +0530

    add note endpoints for the backend

---
+++ backend/controllers/noteController.js
@@ -0,0 +1,49 @@
+const asyncHandler = require("express-async-handler");
+
+const User = require("../models/userModel");
+const Note = require("../models/noteModel");
+const Ticket = require("../models/ticketModel");
+
+const getNotes = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user.id);
+  if (!user) {
+    res.status(400);
+    throw new Error("Unable to find user");
+  }
+
+  const ticket = await Ticket.findById(req.params.ticketId);
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401);
+  }
+
+  const notes = await Note.find({ ticket: req.params.ticketId });
+
+  res.status(200).json(notes);
+});
+
+const addNote = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user.id);
+  if (!user) {
+    res.status(400);
+    throw new Error("Unable to find user");
+  }
+
+  const ticket = await Ticket.findById(req.params.ticketId);
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401);
+  }
+
+  const note = await Note.create({
+    text: req.body.text,
+    isStaff: false, 
+    ticket: req.params.ticketId,
+    user: req.user.id
+  });
+
+  res.status(200).json(note);
+});
+
+module.exports = {
+  getNotes,
+  addNote
+};
--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -1,8 +1,21 @@
-const express= require("express"); 
-const router= express.Router(); 
-const {protect}= require("../middlewear/authMiddleware");
-const {getTickets, createTicket, getTicket, updateTicket, deleteTicket} = require("../controllers/ticketController"); 
+const express = require("express");
+const router = express.Router();
+const { protect } = require("../middlewear/authMiddleware");
+const {
+  getTickets,
+  createTicket,
+  getTicket,
+  updateTicket,
+  deleteTicket,
+} = require("../controllers/ticketController");
+const noteRouter= require("./noteRoute"); 
 
-router.route("/").get(protect, getTickets).post(protect, createTicket); 
-router.route("/:id").get(protect, getTicket).delete(protect, deleteTicket).put(protect, updateTicket); 
-module.exports= router; 
+router.use("/:ticketId/notes", noteRouter); 
+
+router.route("/").get(protect, getTickets).post(protect, createTicket);
+router
+  .route("/:id")
+  .get(protect, getTicket)
+  .delete(protect, deleteTicket)
+  .put(protect, updateTicket);
+module.exports = router;
